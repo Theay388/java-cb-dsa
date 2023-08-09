07ad804e91a9af06c788217cb136869ab2467da7
Theay388 <139104054+Theay388@users.noreply.github.com>
2023-08-09 14:12:35 +0530

    Add files via upload

---
+++ src/lec_10/newjava.java
@@ -0,0 +1,11 @@
+package lec_10;
+
+public class newjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hii ");
+        
+	}         
+
+}
